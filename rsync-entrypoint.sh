f47a5af94e48bb42a3fc43cd98b0965659f557fd
David Kruger <David.Kruger@jdsu.com>
2017-07-13 16:18:15 -0400

    Created the Dockerfile definition for the rsync cron container
    The logic here reuses some of the setup from dkruger/mysql-backup, which
    is similarly based off the dkruger/cron container.

---
+++ rsync-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+# Make sure that the group and users specified by the user exist
+if ! getent group "${RSYNC_GID}" &>/dev/null; then
+    addgroup -g "${RSYNC_GID}" "rsynccron"
+fi
+RSYNC_GROUP="$(getent group "${RSYNC_GID}" | cut -d: -f1)"
+
+if ! getent passwd "${RSYNC_UID}" &>/dev/null; then
+    adduser -u "${RSYNC_UID}" -H "rsynccron" "${RSYNC_GROUP}"
+fi
+RSYNC_USER="$(getent passwd "${RSYNC_UID}" | cut -d: -f1)"
+
+if ! getent group "${RSYNC_GROUP}" | grep "${RSYNC_USER}" &>/dev/null; then
+    addgroup "${RSYNC_USER}" "${RSYNC_GROUP}"
+fi
+
+# Create a rsync script, makes it easier to sudo
+cat << EOF > /run-rsync.sh
+sudo -u "${RSYNC_USER}" -g "${RSYNC_GROUP}" \
+    rsync \
+        ${RSYNC_OPTIONS} \
+        /rsync_src/ \
+        /rsync_dst
+EOF
+chmod +x /run-rsync.sh
+
+# Setup our crontab entry
+export CRONTAB_ENTRY="${RSYNC_CRONTAB} sh /run-rsync.sh"
